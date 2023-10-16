91752fd9c7fa31e54bcd88a4d6ddf904ac0126d2
imght <ghanbartimar@gmail.com>
2023-10-16 12:05:34 +0330

    wip: add whitepaper pdf to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,8 +4,14 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 const Footer = () => {
   return (
     <div className="mx-auto h-44 w-11/12 rounded-lg bg-primary p-8 text-white">
-      <div className="flex  flex-col gap-4">
-        <span>Auction Coin</span>
+      <div className="flex flex-col gap-4">
+        <span className="text-title-medium">Auction Coin</span>
+        <a
+          href="/assets/whitepaper.pdf"
+          className="text-title-small hover:text-gray-300"
+        >
+          Whitepaper
+        </a>
         <div className="flex gap-3">
           <a href="/">
             <TwitterIcon className="hover:text-gray-300" />
